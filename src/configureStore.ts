61ed85a1459d6f26b3b470168bea204ee3fa4907
Abraham Yunes <may7th@gmail.com>
2019-06-18 19:38:01 -0500

    removed some redux dev extension issue when publishing

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -2,16 +2,16 @@ import { createStore,compose, applyMiddleware } from "redux";
 import rootReducer from "./store";
 import { middleware } from "./store/middleware";
 
-
+/*
 declare global {
     interface Window {
 		__REDUX_DEVTOOLS_EXTENSION__?: typeof compose;
     }
 }
-
+*/
 const enhancers = compose(
-	applyMiddleware(...middleware),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	applyMiddleware(...middleware)/*,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/
 );
 const store = createStore(rootReducer,enhancers);
 export default store;
